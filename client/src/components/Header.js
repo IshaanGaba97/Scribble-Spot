acdeb321fbd3a48beda6c0b4505da168e7438ceb
Ishaan Gaba <ishaangaba97@gmail.com>
2023-06-22 00:45:57 +0530

    initial commit

---
+++ client/src/components/Header.js
@@ -0,0 +1,93 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { lightBlue } from "@mui/material/colors";
+import Avatar from "@mui/material/Avatar";
+
+import {
+  Box,
+  AppBar,
+  Toolbar,
+  Button,
+  Typography,
+  Tabs,
+  Tab,
+} from "@mui/material";
+import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { authActions } from "../redux/store";
+import toast from "react-hot-toast";
+const Header = () => {
+  // global state
+  let isLogin = useSelector((state) => state.isLogin);
+  isLogin = isLogin || localStorage.getItem("userId");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  //state
+  const [value, setValue] = useState();
+
+  //logout
+  const handleLogout = () => {
+    try {
+      dispatch(authActions.logout());
+      toast.success("Logout Successfully");
+      navigate("/login");
+      localStorage.clear();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <>
+      <AppBar position="sticky" sx ={{bgcolor: lightBlue[900], padding:1}}>
+        <Toolbar>
+        <Avatar alt="Blog App" src="https://img.freepik.com/premium-vector/letter-b-robot-logo-design-vector-illustration-template_110373-1411.jpg?size=626&ext=jpg&uid=R88677296&ga=GA1.1.1158190123.1681072586&semt=ais" />
+          <Typography variant="subtitle2" fontSize={28} marginLeft={2}>Scribble Spot</Typography>
+          {isLogin && (
+            <Box display={"flex"} marginLeft="auto" marginRight={"auto"}>
+              <Tabs
+                textColor="inherit"
+                value={value}
+                onChange={(e, val) => setValue(val)}
+              >
+                <Tab label="Blogs" LinkComponent={Link} to="/blogs" />
+                <Tab label="My Blogs" LinkComponent={Link} to="/my-blogs" />
+                <Tab
+                  label="Create Blog"
+                  LinkComponent={Link}
+                  to="/create-blog"
+                />
+              </Tabs>
+            </Box>
+          )}
+          <Box display={"flex"} marginLeft="auto">
+            {!isLogin && (
+              <>
+                <Button
+                  sx={{ margin: 1, color: "white" }}
+                  LinkComponent={Link}
+                  to="/login"
+                >
+                  Login
+                </Button>
+                <Button
+                  sx={{ margin: 1, color: "white" }}
+                  LinkComponent={Link}
+                  to="/register"
+                >
+                  Register
+                </Button>
+              </>
+            )}
+            {isLogin && (
+              <Button onClick={handleLogout} sx={{ margin: 1, color: "white" }}>
+                Logout
+              </Button>
+            )}
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
+};
+
+export default Header;
